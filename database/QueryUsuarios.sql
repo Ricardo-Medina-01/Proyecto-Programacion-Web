d68cff9d29222b1659118fa3029e2176f8f6cfc8
RicardoMedina <ricardomedina2302@gmail.com>
2026-04-27 10:03:49 -0500

    DataBase

--- database/QueryUsuarios.sql
+++ database/QueryUsuarios.sql
@@ -2,6 +2,20 @@ CREATE DATABASE Dentista
 GO
 USE Dentista
 
---------------------------------------
---CREACION DE TABLA USUARIO PARA LOGIN
---------------------------------------
+----------------------------------------
+--CREACION DE TABLA USUARIO PARA SISTEMA
+----------------------------------------
+CREATE TABLE Usuarios(
+	usuario char(15) primary key,									--usuario para ingreso al sistema
+	pass char(50),													--contraseña para ingreso al sistema
+	nombre varchar(20),												--nombre real del usuario
+	aPaterno varchar(20),											--apellido paterno
+	aMaterno varchar(20),											--apellido paterno
+	email varchar(30),												--correo (información adicional)
+	tipo char(1) check(tipo in ('A', 'M', 'E', 'I')) default 'I',   --tipo de usuario que ingrese al sistema (Administrador / Medico / Empleado / Invitado)
+	status char(1) check(status in('A', 'I')) default 'A'			--status del usuario (ACTIVO ó INACTIVO)
+)
+GO
+SELECT * FROM Usuarios
+
+INSERT INTO Usuarios(usuario, pass, nombre, aPaterno, aMaterno, email, tipo) VALUES('RicMedSor001', 'adminsistema001', 'Ricardo', 'Medina', 'Soriano', 'ricardomedsor@gmail.com', 'A')
